6733028131ce0b009526050da4a455cfdc1d0259
Sercurio <penalva.louis@gmail.com>
2021-04-08 12:18:11 +0200

    unicafe step1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -10,19 +10,22 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+  console.log(props);
+  const parts = props.parts
   return (
     <div>
-      <Part part={props.part1} exercise={props.exercise1} />
-      <Part part={props.part2} exercise={props.exercise2} />
-      <Part part={props.part3} exercise={props.exercise3} />
+      <Part name={parts[0].name} exercise={parts[0].exercises} />
+      <Part name={parts[1].name} exercise={parts[1].exercises} />
+      <Part name={parts[2].name} exercise={parts[2].exercises} />
     </div>
   )
 }
 
 const Part = (props) => {
+  console.log(props);
   return (
     <p>
-      {props.part} {props.exercise}
+      {props.name} {props.exercise}
     </p>
   )
 }
@@ -36,25 +39,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
 
   return (
     <div>
-      <Header header={course} />
-      <Content part1={part1} exercise1={exercises1} part2={part2} exercise2={exercises2} part3={part3} exercise3={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Header header={course.name} />
+      <Content parts={course.parts} />
+      <Total total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
     </div>
   )
 }
