2bc94d9d9e4d146a3dc67e7a839d9cdd67f0a7f2
Sercurio <penalva.louis@gmail.com>
2021-04-08 14:29:48 +0200

    step2 & step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,28 +8,57 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}> {text} </button>
 )
 
-const Statistics = ({ good, neutral, bad }) => (
-  <div>
-    <p>good: {good}</p>
-    <p>neutral: {neutral}</p>
-    <p>bad: {bad}</p>
-  </div>
-)
+const Statistics = ({ good, neutral, bad, allClicks }) => {
+  const average = () => {
+    return ((good * 1) + (bad * -1)) / allClicks
+  }
+
+  const positiveFeedback = () => {
+    return (good / allClicks) * 100
+  }
+
+  return (
+    <div>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p>all: {allClicks}</p>
+      <p>average: {average()}</p>
+      <p>positive: {positiveFeedback()}%</p>
+    </div>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAllClicks] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setAllClicks(allClicks + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setAllClicks(allClicks + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setAllClicks(allClicks + 1)
+  }
 
   return (
     <div>
       <Header text="Give Feedback" />
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Button handleClick={() => handleGoodClick()} text="good" />
+      <Button handleClick={() => handleNeutralClick()} text="neutral" />
+      <Button handleClick={() => handleBadClick()} text="bad" />
       <Header text="Statistics" />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} allClicks={allClicks} />
     </div>
   )
 }
