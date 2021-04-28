02989a39a46362d8b076f5f0e4db0dc8b2914332
Sercurio <penalva.louis@gmail.com>
2021-04-28 13:52:34 +0200

    add heroku

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 morgan.token('body', (req, res) => {
@@ -81,7 +83,7 @@ const getRandomNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
