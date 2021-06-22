c6b2ec18ae691546130a6c4c6018790d0779b921
Sercurio <penalva.louis@gmail.com>
2021-06-22 12:20:30 +0200

    ex 8.13

---
+++ part8/library/library-frontend/src/components/Authors.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react'
+import { useMutation, useQuery } from '@apollo/client'
+import { ALL_AUTHORS, EDIT_BIRTH } from '../queries'
+
+const Authors = props => {
+  const result = useQuery(ALL_AUTHORS, {
+    pollInterval: 2000,
+  })
+  const [editBirth] = useMutation(EDIT_BIRTH)
+
+  const [authors, setAuthors] = useState([])
+
+  const [authorName, setAuthorName] = useState('')
+  const handleAuthorNameChange = e => {
+    const authorNameInput = e.target.value
+    setAuthorName(authorNameInput)
+  }
+
+  const [authorBirth, setAuthorBirth] = useState('')
+  const handleAuthorBirthChange = e => {
+    const authorBirthInput = e.target.value
+    setAuthorBirth(authorBirthInput)
+  }
+
+  const handleBirthChangeForm = e => {
+    e.preventDefault()
+
+    editBirth({
+      variables: { name: authorName, setBornTo: Number(authorBirth) },
+    })
+  }
+
+  useEffect(() => {
+    if (result.data) {
+      setAuthors(result.data.allAuthors)
+    }
+  }, [result])
+
+  if (!props.show) {
+    return null
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      {authors ? (
+        <table>
+          <tbody>
+            <tr>
+              <th></th>
+              <th>born</th>
+              <th>books</th>
+            </tr>
+            {authors.map(a => (
+              <tr key={a.name}>
+                <td>{a.name}</td>
+                {a.born ? <td>{a.born}</td> : <td></td>}
+                <td>{a.bookCount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>Loading...</p>
+      )}
+      <h2>Set birthyear</h2>
+      <form onSubmit={handleBirthChangeForm}>
+        <div>
+          name
+          {authors ? (
+            <select value={authorName} onChange={handleAuthorNameChange}>
+              {authors.map(author => (
+                <option value={author.name}>{author.name}</option>
+              ))}
+            </select>
+          ) : null}
+        </div>
+        <div>
+          birth
+          <input value={authorBirth} onChange={handleAuthorBirthChange} />
+        </div>
+        <button type='submit'>changeBirth</button>
+      </form>
+    </div>
+  )
+}
+
+export default Authors
---
+++ part8/library/library-frontend/src/queries.js
@@ -0,0 +1,54 @@
+import { gql } from '@apollo/client'
+
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      id
+      bookCount
+    }
+  }
+`
+
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      published
+      author
+      id
+      genres
+    }
+  }
+`
+export const ADD_BOOK = gql`
+  mutation createBook(
+    $title: String!
+    $author: String!
+    $published: Int!
+    $genres: [String]
+  ) {
+    addBook(
+      title: $title
+      published: $published
+      author: $author
+      genres: $genres
+    ) {
+      title
+      published
+      author
+      id
+      genres
+    }
+  }
+`
+
+export const EDIT_BIRTH = gql`
+  mutation editBirth($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo) {
+      name
+      born
+    }
+  }
+`
