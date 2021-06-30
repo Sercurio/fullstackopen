0239bd9bd65c2cec1ad901d5f528d5559419fbe9
louis.penalva <penalva.louis@gmail.com>
2021-06-30 14:20:08 +0200

    part9 continued

--- part9/calculator/exerciseCalculator.ts
+++ part9/calculator/exerciseCalculator.ts
@@ -1,19 +1,61 @@
 interface Result {
-  periodLength : number,
-  trainingDays : number,
-  success : 
-  
+  periodLength: number
+  trainingDays: number
+  success: boolean
+  rating: number
+  ratingDescription: string
+  target: number
+  average: number
 }
 
-const calculateExercises = (dailyExercicesHours: Array<number>): Array<any> => {
+const calculateExercises = (dailyExercicesHours: Array<number>): Result => {
+  if (dailyExercicesHours.length == 0) {
+    throw Error('empty array')
+  }
   const target = 2
-  const numberOfDays = dailyExercicesHours.length
-  const numberOfTrainingDays = dailyExercicesHours.filter(d => {
-    d >= 0 && d != 0
-  }).length
-  const average =
-    dailyExercicesHours.reduce((prev, curr) => prev + curr) / numberOfDays
-  const success = average >= target ? true : false
 
-  return null
+  const periodLength = dailyExercicesHours.length - 1
+
+  const trainingDays =
+    dailyExercicesHours.filter(d => d >= 0 && d != 0).length - 1
+
+  const average =
+    dailyExercicesHours.reduce((prev, curr) => prev + curr) / periodLength
+
+  const success = trainingDays >= target ? true : false
+
+  const rating = (3 * trainingDays) / 7
+
+  var ratingDescription: string
+  if (rating >= 0 && rating < 1) {
+    ratingDescription = "It's bad, ur not gonna get cheeks this summer"
+  } else if (rating >= 1 && rating < 2) {
+    ratingDescription = 'Good point sir and body :wink wink:'
+  } else {
+    ratingDescription = 'Ur perfect, change nothing'
+  }
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  }
+}
+
+try {
+  var i = 0
+  var dailyExercicesHours: Array<number> = []
+  process.argv.forEach(arg => {
+    if (i > 1) {
+      dailyExercicesHours.push(Number(arg))
+    }
+    i++
+  })
+  console.log(calculateExercises(dailyExercicesHours))
+} catch (e) {
+  console.error(e)
 }
