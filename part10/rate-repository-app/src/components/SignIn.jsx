38d61b3c2104d36c31df88db13e9781eba458b1f
lpenalva <lpenalva@tibco.fr>
2021-07-28 18:57:38 +0200

    question 10.13 processing

--- part10/rate-repository-app/src/components/SignIn.jsx
+++ part10/rate-repository-app/src/components/SignIn.jsx
@@ -10,6 +10,7 @@ import FormikTextInput from './FormikTextInput';
 import theme from '../theme';
 
 import * as yup from 'yup';
+import useSignIn from '../hooks/useSignIn';
 
 const validationSchema = yup.object().shape({
   username: yup.string().required('Username is required'),
@@ -17,8 +18,17 @@ const validationSchema = yup.object().shape({
 });
 
 const SignIn = () => {
-  const onSubmit = values => {
-    console.log(values);
+  const [signIn, result] = useSignIn();
+
+  const onSubmit = async values => {
+    const { username, password } = values;
+
+    try {
+      const { data } = await signIn({ username, password });
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   const initialValues = {
---
+++ part10/rate-repository-app/src/hooks/useRepositories.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+import { useQuery } from '@apollo/client';
+
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+  const { data, error, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  });
+
+  const [repositories, setRepositories] = useState();
+
+  useEffect(() => {
+    if (data) {
+      setRepositories(data.repositories);
+    } else if (error) {
+      console.log(error);
+    }
+  }, [data, error]);
+
+  return { repositories, loading };
+};
+
+export default useRepositories;
