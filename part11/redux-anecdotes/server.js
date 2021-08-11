64eef0acb65dc0133012cafd2802a3e8436d2d78
lpenalva <lpenalva@tibco.fr>
2021-08-11 13:21:33 +0200

    adding health endpoint

--- part11/redux-anecdotes/server.js
+++ part11/redux-anecdotes/server.js
@@ -5,6 +5,12 @@ const middlewares = jsonServer.defaults({ static: './build' })
 
 server.use(middlewares)
 server.use(router)
+
+server.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 server.listen(3001, () => {
+  // eslint-disable-next-line no-console
   console.log('JSON Server is running')
 })
